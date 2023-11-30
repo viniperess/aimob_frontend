470fe8115168fb06bd14379604f0a3ee4d7f373e
Vinicius Peres <viniciusgperes@hotmail.com>
2023-11-29 22:45:54 -0300

    feat: route home

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,14 @@ import { Fragment } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
+import Home from "../pages/Home";
 
 const RoutesApp: React.FC = () => {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Fragment>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
         </Routes>
