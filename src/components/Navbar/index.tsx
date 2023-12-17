2092182684b0b52fd1a479a2b1d3c96ea986ee56
Vinicius Peres <viniciusgperes@hotmail.com>
2023-12-16 23:23:39 -0300

    update file

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -43,7 +43,7 @@ const Navbar: React.FC = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <a className="nav-link active" aria-current="page" href="/signin">
                 Home
               </a>
             </li>
