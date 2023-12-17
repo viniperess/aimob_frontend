81c8acf2e04bcb13f7699d762adf9667bf76fddc
Vinicius Peres <viniciusgperes@hotmail.com>
2023-12-17 19:32:26 -0300

    api file, vercel cloud

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api/v1/",
+  // baseURL: "http://localhost:3001/api/v1/",
+  baseURL: "https://aimob-backend-viniperess.vercel.app/api/v1/",
 });
 
 api.interceptors.request.use(async (config) => {
