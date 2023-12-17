dfffb321b52930c221fa509c31ba1b11dc5d5262
Vinicius Peres <viniciusgperes@hotmail.com>
2023-12-16 22:18:44 -0300

    feat: api backend

--- src/service/api.ts
+++ src/service/api.ts
@@ -4,11 +4,26 @@ const api = axios.create({
   baseURL: "http://localhost:3001/api/v1/",
 });
 
-api.interceptors.request.use((config) => {
+api.interceptors.request.use(async (config) => {
   const token = localStorage.getItem("userToken");
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  const cep = config.params?.cep;
+  if (cep) {
+    try {
+      const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+      const addressData = response.data;
+
+      config.params.street = addressData.logradouro || "";
+      config.params.district = addressData.bairro || "";
+      config.params.city = addressData.localidade || "";
+      config.params.state = addressData.uf || "";
+    } catch (error) {
+      console.error("Erro ao buscar endereço:", error);
+    }
   }
 
   return config;
