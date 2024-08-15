3c2965a56d4c29fff905ffb20921c1e64dae52e1
Vinicius Peres <viniciusgperes@hotmail.com>
2024-08-15 18:10:53 -0300

    feat: new funtions and styles

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -8,7 +8,9 @@ const SignUp: React.FC = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [cpf, setCpf] = useState("");
-  const [birthdate, setBirthdate] = useState("");
+  const [city, setCity] = useState("");
+  const [phone, setPhone] = useState("");
+  const [creci, setCreci] = useState("");
   const [password, setPassword] = useState("");
   const [passwordTwo, setPasswordTwo] = useState("");
   const [error, setError] = useState("");
@@ -22,7 +24,7 @@ const SignUp: React.FC = () => {
   const handleRegister = async (e: FormEvent) => {
     e.preventDefault();
 
-    if (!user || !name || !email || !cpf || !birthdate || !password || !passwordTwo) {
+    if (!user || !name || !email || !cpf || !password || !passwordTwo || !creci || !city || !phone) {
       setError("Preencha todos os campos!");
       return;
     } else if (!emailRegex.test(email)) {
@@ -42,7 +44,9 @@ const SignUp: React.FC = () => {
         name,
         email,
         cpf,
-        birthdate,
+        city,
+        phone,
+        creci,
         password: password,
       });
       navigate("/signin");
@@ -65,91 +69,126 @@ const SignUp: React.FC = () => {
           id="form"
         >
           <h3>Registro</h3>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            value={name}
-            placeholder="Nome"
-            onChange={(e) => [setName(e.target.value), setError("")]}
-            className={error && !name ? "errors" : "input_control"}
-          />
-          <input
-            type="text"
-            name="user"
-            id="user"
-            value={user}
-            placeholder="Usuário"
-            onChange={(e) => [setUser(e.target.value), setError("")]}
-            className={error && !user ? "errors" : "input_control"}
-          />
-          <input
-            type="email"
-            name="email"
-            id="email"
-            value={email}
-            placeholder="Email"
-            onChange={(e) => [setEmail(e.target.value), setError("")]}
-            className={
-              (error && !email) ||
-              (!emailRegex.test(email) && email.length !== 0)
-                ? "errors"
-                : "input_control"
-            }
-          />
-          <input
-            type="text"
-            name="cpf"
-            id="cpf"
-            value={cpf}
-            placeholder="Cpf"
-            onChange={(e) => [setCpf(e.target.value), setError("")]}
-            className={error && !cpf ? "errors" : "input_control"}
-          />
-          <input
-            type="date"
-            name="birthdate"
-            id="birthdate"
-            value={birthdate}
-            placeholder="Data de Nascimento"
-            onChange={(e) => [setBirthdate(e.target.value), setError("")]}
-            className={error && !birthdate ? "errors" : "input_control"}
-          />
-
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => [setPassword(e.target.value), setError("")]}
-            className={
-              (error && !password) ||
-              (password.length < 8 && password.length !== 0)
-                ? "errors"
-                : "input_control"
-            }
-          />
-          <input
-            type="password"
-            name="passwordTwo"
-            id="passwordTwo"
-            value={passwordTwo}
-            placeholder="Password Confirm"
-            onChange={(e) => [setPasswordTwo(e.target.value), setError("")]}
-            className={
-              (error && !passwordTwo) || password !== passwordTwo
-                ? "errors"
-                : "input_control"
-            }
-          />
-          <label className="classErrors">{error}</label>
-          <button type="submit" className="btnRegister">
-            Registrar-se
-          </button>
-          <p className="log">
-            Já possui uma conta? <a href="/SignIn">Faça Login</a>
-          </p>
+          <div className="mb-0">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={email}
+                placeholder="Email"
+                onChange={(e) => [setEmail(e.target.value), setError("")]}
+                className={`form-control ${(error && !email) || (!emailRegex.test(email) && email.length !== 0) ? "is-invalid" : ""}`}
+              />
+            </div>
+          <div className="row">
+            <div className="col">
+              <input
+                type="text"
+                name="name"
+                id="name"
+                value={name}
+                placeholder="Nome"
+                onChange={(e) => [setName(e.target.value), setError("")]}
+                className={`form-control ${error && !name ? "is-invalid" : ""}`}
+              />
+              </div>
+       
+            <div className="col">
+              <input
+                type="text"
+                name="user"
+                id="user"
+                value={user}
+                placeholder="Usuário"
+                onChange={(e) => [setUser(e.target.value), setError("")]}
+                className={`form-control ${error && !user ? "is-invalid" : ""}`}
+              />
+            </div>
+            </div>
+           
+            <div className="row">
+            <div className="col">
+              <input
+                type="text"
+                name="cpf"
+                id="cpf"
+                value={cpf}
+                placeholder="Cpf"
+                onChange={(e) => [setCpf(e.target.value), setError("")]}
+                className={`form-control ${error && !cpf ? "is-invalid" : ""}`}
+              />
+            </div>
+            <div className="col">
+              <input
+                type="text"
+                name="city"
+                id="city"
+                value={city}
+                placeholder="Cidade"
+                onChange={(e) => [setCity(e.target.value), setError("")]}
+                className={`form-control ${error && !city ? "is-invalid" : ""}`}
+              />
+            </div>
+            </div>
+            <div className="row">
+            <div className="col">
+              <input
+                type="text"
+                name="phone"
+                id="phone"
+                value={phone}
+                placeholder="Telefone"
+                onChange={(e) => [setPhone(e.target.value), setError("")]}
+                className={`form-control ${error && !phone ? "is-invalid" : ""}`}
+              />
+            </div>
+            <div className="col">
+              <input
+                type="text"
+                name="creci"
+                id="creci"
+                value={creci}
+                placeholder="Creci"
+                onChange={(e) => [setCreci(e.target.value), setError("")]}
+                className={`form-control ${error && !creci ? "is-invalid" : ""}`}
+              />
+            </div>
+            </div>
+            <div className="row">
+            <div className="col">
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => [setPassword(e.target.value), setError("")]}
+                className={`form-control ${(error && !password) || (password.length < 8 && password.length !== 0) ? "is-invalid" : ""}`}
+              />
+            </div>
+            <div className="col">
+              <input
+                type="password"
+                name="passwordTwo"
+                id="passwordTwo"
+                value={passwordTwo}
+                placeholder="Password Confirm"
+                onChange={(e) => [setPasswordTwo(e.target.value), setError("")]}
+                className={`form-control ${(error && !passwordTwo) || password !== passwordTwo ? "is-invalid" : ""}`}
+              />
+            </div>
+            </div>
+            <div className="text-center">
+              <label className="text-danger">{error}</label>
+            </div>
+            <div className="d-grid">
+              <button type="submit" className="btn bg-white text-primary">
+                Registrar-se
+              </button>
+            </div>
+            <p className="text-center text-white">
+              Já possui uma conta? <a href="/SignIn">Faça Login</a>
+              </p>
         </form>
       </div>
       <div className="image"></div>
