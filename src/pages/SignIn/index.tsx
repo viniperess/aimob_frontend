ad2ff61d470cfc26a894760361fbb951677897cc
Vinicius Peres <viniciusgperes@hotmail.com>
2024-08-15 18:11:05 -0300

    feat: new funtions and styles

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -2,7 +2,7 @@ import React, { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../../service/api";
 import "./styles.css";
-
+import Logo from "../../assets/images/aimoblogo_azul.png";
 const SignIn: React.FC = () => {
   const [user, setUser] = useState("");
   const [password, setPassword] = useState("");
@@ -31,48 +31,80 @@ const SignIn: React.FC = () => {
   };
 
   return (
-    <div className="containerLogin">
-      <div className="boxFormLogin">
-        <h1 className="ola">Olá,</h1>
-        <p className="textCabecalho">Para continuar navegando de forma segura, efetue o login</p>
+    <div
+      className="container-fluid d-flex"
+      style={{height: "100vh"
+      }}
+    >
+      <div className="col text-start m-4">
+      <div
+        className="boxFormLogin align-items-start m-4"
+      >
+        <h1 className="ola mx-4">
+          Olá,
+        </h1>
+        <p className="text-start mx-4">
+          Para continuar navegando de forma segura, efetue o login
+        </p>
 
-        <form id="formLogin" method="post"              className="formLogin" onSubmit={handleLogin}>
+        <form
+          id="formLogin"
+          method="post"
+          className="formLogin m-4"
+          onSubmit={handleLogin}
+        >
           <h3>Login</h3>
-          <input
-            type="text"
-            name="user"
-            id="userLogin"
-            value={user}
-            placeholder="Usuário"
-            onChange={(e) => [setUser(e.target.value), setError("")]}
-            className={
-              error && !user ? "form-control is-invalid" : "form-control"
-            }
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Senha"
-            id="passwordLogin"
-            value={password}
-            onChange={(e) => [setPassword(e.target.value), setError("")]}
-            className={
-              error && !password ? "form-control is-invalid" : "form-control"
-            }
-          />
-          <label className="error">
-            {error}
-          </label>
-          <button type="submit" className="btnLogin">
+          <div className="row">
+            <div className="col">
+              <label htmlFor="userLogin" className="form-label">
+                Usuário
+              </label>
+              <input
+                type="text"
+                name="user"
+                id="userLogin"
+                value={user}
+                placeholder="Usuário"
+                onChange={(e) => [setUser(e.target.value), setError("")]}
+                className={
+                  error && !user ? "form-control is-invalid" : "form-control"
+                }
+              />
+            </div>
+            <div className="col">
+              <label htmlFor="passwordLogin" className="form-label">
+                Senha
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Senha"
+                id="passwordLogin"
+                value={password}
+                onChange={(e) => [setPassword(e.target.value), setError("")]}
+                className={
+                  error && !password
+                    ? "form-control is-invalid"
+                    : "form-control"
+                }
+              />
+            </div>
+          </div>
+          <div className="text-center">
+            <label className="text-danger">{error}</label>
+          </div>
+          <button type="submit" className="btnLogin btn-primary w-100">
             Logar-se
           </button>
-          <p className="logLogin">
-            Novo por aqui? <a href="/SignUp">Registre-se</a>
+          <p className="text-center logLogin">
+            Novo por aqui? <a href="/SignUp">Registre-se</a> ou <a href="http:/forgot_password">Esqueceu a senha</a>
           </p>
+          
         </form>
       </div>
-      <div className="imageLogin">
-        {/* <img src={Logo} alt="Aimob logo" /> */}
+      </div>
+      <div className="page-container imageLogin col-6">
+        <img src={Logo} alt="Aimob logo" />
       </div>
     </div>
   );
