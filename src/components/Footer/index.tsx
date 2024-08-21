c0b8424929a5fa623016cf9db6624ee5efc63fdd
Vinicius Peres <viniciusgperes@hotmail.com>
2024-08-21 20:27:25 -0300

    fix: update footer icons

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,83 +1,60 @@
 import React from "react";
-// import { library } from '@fortawesome/fontawesome-svg-core';
-// import { fab } from '@fortawesome/free-brands-svg-icons';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 // Adicione os ícones que você está usando
-// library.add(fab.faFacebookF, fab.faTwitter, fab.faGoogle, fab.faInstagram, fab.faLinkedinIn, fab.faGithub);
+library.add(fab.faFacebookF, fab.faTwitter, fab.faGoogle, fab.faInstagram, fab.faLinkedinIn, fab.faGithub, fab.faDiscord);
 
 const Footer: React.FC = () => {
   return (
-
     <footer className="bg-primary text-white text-center">
       {/* Grid container */}
       <div className="container p-4 pb-0">
         {/* Section: Social media */}
         <section className="mb-4">
-          {/* Facebook */}
+          {/* Discord */}
           <a
             data-mdb-ripple-init
             className="btn text-white btn-floating m-1"
-            style={{ backgroundColor: "#3b5998" }}
-            href="#!"
+            style={{ backgroundColor: "#6c77f7" }}  // Cor oficial do Discord
+            href="https://discord.gg/W2SXNRr7"
             role="button"
           >
-            {/* <FontAwesomeIcon icon={["fab", "facebook-f"]} /> */}
-          </a>
-
-          {/* Twitter */}
-          <a
-            data-mdb-ripple-init
-            className="btn text-white btn-floating m-1"
-            style={{ backgroundColor: "#55acee" }}
-            href="#!"
-            role="button"
-          >
-            {/* <FontAwesomeIcon icon={["fab", "twitter"]} /> */}
-          </a>
-
-          {/* Google */}
-          <a
-            data-mdb-ripple-init
-            className="btn text-white btn-floating m-1"
-            style={{ backgroundColor: "#dd4b39" }}
-            href="#!"
-            role="button"
-          >
-            {/* <FontAwesomeIcon icon={["fab", "google"]} /> */}
+            <FontAwesomeIcon icon={["fab", "discord"]} />
           </a>
 
           {/* Instagram */}
           <a
             data-mdb-ripple-init
             className="btn text-white btn-floating m-1"
-            style={{ backgroundColor: "#ac2bac" }}
-            href="#!"
+            style={{ backgroundColor: "#E4405F" }}  // Cor oficial do Instagram
+            href="https://instagram.com/viniperess_"
             role="button"
           >
-            {/* <FontAwesomeIcon icon={["fab", "instagram"]} /> */}
+            <FontAwesomeIcon icon={["fab", "instagram"]} />
           </a>
 
           {/* Linkedin */}
           <a
             data-mdb-ripple-init
             className="btn text-white btn-floating m-1"
-            style={{ backgroundColor: "#0082ca" }}
-            href="#!"
+            style={{ backgroundColor: "#50a3f7" }}  // Cor oficial do LinkedIn
+            href="https://www.linkedin.com/in/vin%C3%ADciusgperes/"
             role="button"
           >
-            {/* <FontAwesomeIcon icon={["fab", "linkedin-in"]} /> */}
+            <FontAwesomeIcon icon={["fab", "linkedin-in"]} />
           </a>
 
           {/* Github */}
           <a
             data-mdb-ripple-init
             className="btn text-white btn-floating m-1"
-            style={{ backgroundColor: "#333333" }}
-            href="#!"
+            style={{ backgroundColor: "#ffff" }}  // Cor oficial do GitHub
+            href="https://github.com/viniperess"
             role="button"
           >
-            {/* <FontAwesomeIcon icon={["fab", "github"]} /> */}
+            <FontAwesomeIcon icon={["fab", "github"]} />
           </a>
         </section>
         {/* Section: Social media */}
@@ -85,7 +62,7 @@ const Footer: React.FC = () => {
       {/* Grid container */}
 
       {/* Copyright */}
-      <div className="text-center p-3" style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}>
+      <div className="text-center p-3" style={{ backgroundColor: "rgba(0, 0, 0, 0.05)", color: "white" }}>
         © 2023 Copyright
       </div>
       {/* Copyright */}
