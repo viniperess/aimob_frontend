b81700ac6457e25e1150d4ff9524b99f077599d7
Vinicius Peres <viniciusgperes@hotmail.com>
2024-08-24 20:21:50 -0300

    fit: utc times for front

--- src/components/Notification/index.tsx
+++ src/components/Notification/index.tsx
@@ -6,6 +6,9 @@ import IconNotification from "../../assets/images/icons8-lembrete-de-compromisso
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { Task } from "../../types/task";
+import { Col, Row } from "react-bootstrap";
+import { format } from "date-fns";
+
 
 const NotificationDropdown: React.FC = () => {
   const [notifications, setNotifications] = useState<NotificationType[]>([]);
@@ -137,49 +140,62 @@ const NotificationDropdown: React.FC = () => {
         </div>
       )}
       <Modal show={modalOpen} onHide={() => setModalOpen(false)} centered>
-        <Modal.Header closeButton>
+        <Modal.Header closeButton className="bg-primary text-white">
           <Modal.Title>Detalhes da Tarefa</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           {taskDetails ? (
             <>
-              <p>
-                <strong>Status:</strong> {taskDetails.status}
-              </p>
-              <p>
-                <strong>Descrição:</strong> {taskDetails.description}
-              </p>
-              {taskDetails.realEstate && (
+              <Row className="mb-4">
+                <Col md={6}>
+                  <h5>Status</h5>
+                  <p>{taskDetails.status}</p>
+                </Col>
+                <Col md={6}>
+                  <h5>Descrição</h5>
+                  <p>{taskDetails.description}</p>
+                </Col>
+              </Row>
+
+              {taskDetails.contact && taskDetails.realEstate && (
                 <>
-                  <h5>Imóvel:</h5>
-                  <p>ID do Imóvel: {taskDetails.realEstate.id}</p>
-                  <p>
-                    Endereço: {taskDetails.realEstate.street},{" "}
-                    {taskDetails.realEstate.number},{" "}
-                    {taskDetails.realEstate.city}
-                  </p>
-                </>
-              )}
-              {taskDetails.contact && (
-                <>
-                  <h5>Contato:</h5>
-                  <p>ID do Contato: {taskDetails.contact.id}</p>
-                  <p>Nome: {taskDetails.contact.name}</p>
-                  <p>Email: {taskDetails.contact.email}</p>
-                  <p>Telefone: {taskDetails.contact.phone}</p>
+                  <Row className="mb-4">
+                    <Col>
+                      <h5>Contato</h5>
+                      <p>ID do Contato: {taskDetails.contact.id}</p>
+                      <p>Nome: {taskDetails.contact.name}</p>
+                      <p>Email: {taskDetails.contact.email}</p>
+                      <p>Telefone: {taskDetails.contact.phone}</p>
+                    </Col>
+                    <Col md={6}>
+                      {" "}
+                      <h5>Imóvel</h5>
+                      <p>ID do Imóvel: {taskDetails.realEstate.id}</p>
+                      <p>
+                        {taskDetails.realEstate.street},{" "}
+                        {taskDetails.realEstate.number},{" "}
+                        {taskDetails.realEstate.city}
+                      </p>
+                    </Col>
+                  </Row>
                 </>
               )}
               {taskDetails.appointment && taskDetails.appointment.visitDate && (
                 <>
-                  <h5>Agendamento:</h5>
-                  <p>ID do Agendamento: {taskDetails.appointment.id}</p>
-                  <p>
-                    Data e Hora:{" "}
-                    {new Date(
-                      taskDetails.appointment.visitDate
-                    ).toLocaleString()}
-                  </p>
-                  <p>Observação: {taskDetails.appointment.observation}</p>
+                  <Row className="mb-4">
+                    <Col>
+                      <h5>Agendamento</h5>
+                      <p>ID do Agendamento: {taskDetails.appointmentId}</p>
+                      <p>
+                        Data e Hora:{" "}
+                        {format(
+                          new Date(taskDetails.appointment.visitDate),
+                          "dd/MM/yyyy, HH:mm"
+                        )}
+                      </p>
+                      <p>Observação: {taskDetails.appointment.observation}</p>
+                    </Col>
+                  </Row>
                 </>
               )}
             </>
