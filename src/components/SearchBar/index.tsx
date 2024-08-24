2f288bd1e13256021de290e5e769a0c07b105c6b
Vinicius Peres <viniciusgperes@hotmail.com>
2024-08-24 20:22:25 -0300

    fix: styles in the search

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,37 +1,31 @@
 import React, { FormEvent, useState } from "react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
-import { useNavigate } from "react-router-dom";
 
 import "./styles.css";
-
-const SearchBar: React.FC = () => {
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+}
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   const [query, setQuery] = useState("");
-
-  const navigate = useNavigate();
-
   const handleSearch = (e: FormEvent) => {
     e.preventDefault();
-    navigate(`/search?query=${query}`);
+    onSearch(query);
   };
 
   return (
-    <form className="d-flex mx-auto" role="search" onSubmit={handleSearch}>
+    <form className="d-flex" role="search" onSubmit={handleSearch}>
       <div className="searchIcon input-group">
         <input
-          className="pesquisa rounded-end text-light  me-1"
-         
+          className="pesquisa rounded-end me-1"
           type="search"
           placeholder="Encontre Imoveis"
           aria-label="Search"
           value={query}
-          onChange={(e) => setQuery(e.target.value)}
+          onChange={(e) => {
+            setQuery(e.target.value);
+            onSearch(e.target.value);
+          }}
         />
-        <button
-          className="btn text-light btn-outline-info rounded bg-primary " 
-          type="submit"
-        >
-          Pesquisar
-        </button>
         <MagnifyingGlassIcon style={{ color: "#0d6efd", width: "10%" }} />
       </div>
     </form>
