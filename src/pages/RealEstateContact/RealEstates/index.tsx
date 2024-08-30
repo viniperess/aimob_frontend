6d598e89ee67e3188ad7bf6d904b27dddd9c92ce
Vinicius Peres <viniciusgperes@hotmail.com>
2024-08-30 15:11:44 -0300

    fix: realestates search with type and scroll page for search

--- src/pages/RealEstateContact/RealEstates/index.tsx
+++ src/pages/RealEstateContact/RealEstates/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import api from "../../../service/api";
 import { RealEstateType } from "../../../types/realEstate";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -6,11 +6,14 @@ import { faBed, faShower, faRuler } from "@fortawesome/free-solid-svg-icons";
 import "./styles.css";
 import SearchBar from "../../../components/SearchBar";
 import FooterContact from "../../../components/FooterContact";
+
 const RealEstateContact: React.FC = () => {
   const [realEstates, setRealEstates] = useState<RealEstateType[]>([]);
   const [filteredRealEstates, setFilteredRealEstates] = useState<
     RealEstateType[]
   >([]);
+  const [searchPerformed, setSearchPerformed] = useState(false);
+  const searchBarRef = useRef<HTMLDivElement>(null);
 
   const getRealEstates = async () => {
     try {
@@ -24,15 +27,17 @@ const RealEstateContact: React.FC = () => {
 
   const handleSearch = async (query: string) => {
     if (query === "") {
-      setFilteredRealEstates(realEstates); // Mostra todos se a pesquisa estiver vazia
+      setFilteredRealEstates(realEstates);
+      setSearchPerformed(false);
     } else {
       try {
         const response = await api.get(`/realestates/search?type=${query}`);
-        console.log("Resposta da pesquisa:", response.data);
         setFilteredRealEstates(response.data.length > 0 ? response.data : []);
+        setSearchPerformed(true);
       } catch (error) {
         console.error("Failed to search real estates", error);
         setFilteredRealEstates([]);
+        setSearchPerformed(true);
       }
     }
   };
@@ -40,6 +45,19 @@ const RealEstateContact: React.FC = () => {
   useEffect(() => {
     getRealEstates();
   }, []);
+
+  const handleSearchBarClick = () => {
+    if (searchBarRef.current) {
+      const elementPosition =
+        searchBarRef.current.getBoundingClientRect().top + window.pageYOffset;
+      const offsetPosition = elementPosition + 150;
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
     <>
       <div className="hero-image">
@@ -47,13 +65,18 @@ const RealEstateContact: React.FC = () => {
           <div className="logo-column"></div>
           <div className="message-column">
             <p className="text-white">
-              Quer anunciar seu imóvel?<br/> {" "}
-              Entre em contato: <strong className="text-white">(53) 9999-9999</strong>
+              Quer anunciar seu imóvel?
+              <br /> Entre em contato:{" "}
+              <strong className="text-white">(53) 9999-9999</strong>
             </p>
           </div>
         </div>
 
-        <div className="hero-text">
+        <div
+          className="hero-text"
+          ref={searchBarRef}
+          onClick={handleSearchBarClick}
+        >
           <h1 className="hero-title">Aimob</h1>
           <p className="hero-subtitle">
             Seu lugar para encontrar o imóvel dos seus sonhos
@@ -61,58 +84,126 @@ const RealEstateContact: React.FC = () => {
           <SearchBar onSearch={handleSearch} />
         </div>
       </div>
+
       <div className="container d-flex align-items-center flex-wrap">
         <div className="property-container">
-        <div className="row col my-3">
-          <h1 className="col text-center m-3" style={{fontSize: "3rem", fontWeight: "bold"}}>Imóveis à venda</h1>
-        </div>
-        <div className="container row justify-content-center mb-2">
-          {filteredRealEstates.length === 0 && (
-            <>
-              <div className="no-results text-center">
-                <h2>Sem imóveis disponíveis</h2>
+          <div className="row col my-3">
+            <h1
+              className="col text-center m-3"
+              style={{ fontSize: "3rem", fontWeight: "bold" }}
+            >
+              Imóveis à venda
+            </h1>
+          </div>
+
+          <div
+            className={`container row justify-content-center mb-2 ${
+              filteredRealEstates.length === 1 ? "single-item" : ""
+            }`}
+          >
+            {searchPerformed && (
+              <div
+                className="search-results text-center"
+                style={{
+                  backgroundColor: "#f8f9fa",
+                  padding: "20px",
+                  borderRadius: "5px",
+                }}
+              >
+                <h2 className="justify-content-center text-center">
+                  {filteredRealEstates.length > 0
+                    ? `${filteredRealEstates.length} ${
+                        filteredRealEstates.length === 1
+                          ? "imóvel encontrado"
+                          : "imóveis encontrados"
+                      }`
+                    : "Sem imóveis disponíveis"}
+                </h2>
                 <p>
-                  Não encontramos imóveis para sua busca. Que tal dar uma olhada
-                  em outros imóveis disponíveis?
+                  {filteredRealEstates.length > 0
+                    ? "Aqui estão os imóveis que correspondem à sua busca:"
+                    : "Não encontramos imóveis para sua busca. Que tal dar uma olhada em outros imóveis disponíveis?"}
                 </p>
+                <hr className="divider my-4" />
               </div>
-              <hr className="divider my-4" />
-            </>
-          )}
-          {realEstates.map((realEstate) => (
-            <div className="col-md-4 col-sm-6 col-12" key={realEstate.id}>
-              <div className="card">
-                <a href={`/realEstateByContact/${realEstate.id}`}>
-                  <img
-                    src={realEstate.images[0]}
-                    className="card-img"
-                    alt={realEstate.description}
-                  />
-                  <div className="card-img-overlay text-white d-flex justify-content-between align-items-start">
-                    <h6 className="card-title-h5 text-white">DESTAQUE</h6>
-                    <p className="card-text-comprar">COMPRAR</p>
+            )}
+
+            {filteredRealEstates.length > 0
+              ? filteredRealEstates.map((realEstate) => (
+                  <div className="col-md-4 col-sm-6 col-12" key={realEstate.id}>
+                    <div className="card">
+                      <a href={`/realEstateByContact/${realEstate.id}`}>
+                        <img
+                          src={realEstate.images[0]}
+                          className="card-img"
+                          alt={realEstate.description}
+                        />
+                        <div className="card-img-overlay text-white d-flex justify-content-between align-items-start">
+                          <h6 className="card-title-h5 text-white">DESTAQUE</h6>
+                          <p className="card-text-comprar">COMPRAR</p>
+                        </div>
+                      </a>
+                      <div className="card-body border secundary text-left">
+                        <h5 className="card-title text-left">
+                          {realEstate.street}, {realEstate.number}
+                        </h5>
+                        <p className="card-text text-left">
+                          {realEstate.city} - {realEstate.state}
+                        </p>
+                        <p className="card-text text-left">
+                          {realEstate.bedrooms} <FontAwesomeIcon icon={faBed} />{" "}
+                          | {realEstate.bathrooms}{" "}
+                          <FontAwesomeIcon icon={faShower} /> |{" "}
+                          {realEstate.totalArea}m²{" "}
+                          <FontAwesomeIcon icon={faRuler} />
+                        </p>
+                        <div className="text-left">
+                          <h5 className="text-primary">
+                            R$ {realEstate.salePrice}
+                          </h5>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                </a>
-                <div className="card-body border secundary text-left">
-                  <h5 className="card-title text-left">
-                    {realEstate.street}, {realEstate.number}
-                  </h5>
-                  <p className="card-text text-left">
-                    {realEstate.city} - {realEstate.state}
-                  </p>
-                  <p className="card-text text-left">
-                    {realEstate.bedrooms} <FontAwesomeIcon icon={faBed} /> |{" "}
-                    {realEstate.bathrooms} <FontAwesomeIcon icon={faShower} /> |{" "}
-                    {realEstate.totalArea}m² <FontAwesomeIcon icon={faRuler} />
-                  </p>
-                  <div className="text-left">
-                    <h5 className="text-primary">R$ {realEstate.salePrice}</h5>
+                ))
+              : realEstates.map((realEstate) => (
+                  <div className="col-md-4 col-sm-6 col-12" key={realEstate.id}>
+                    <div className="card">
+                      <a href={`/realEstateByContact/${realEstate.id}`}>
+                        <img
+                          src={realEstate.images[0]}
+                          className="card-img"
+                          alt={realEstate.description}
+                        />
+                        <div className="card-img-overlay text-white d-flex justify-content-between align-items-start">
+                          <h6 className="card-title-h5 text-white">DESTAQUE</h6>
+                          <p className="card-text-comprar">COMPRAR</p>
+                        </div>
+                      </a>
+                      <div className="card-body border secundary text-left">
+                        <h5 className="card-title text-left">
+                          {realEstate.street}, {realEstate.number}
+                        </h5>
+                        <p className="card-text text-left">
+                          {realEstate.city} - {realEstate.state}
+                        </p>
+                        <p className="card-text text-left">
+                          {realEstate.bedrooms} <FontAwesomeIcon icon={faBed} />{" "}
+                          | {realEstate.bathrooms}{" "}
+                          <FontAwesomeIcon icon={faShower} /> |{" "}
+                          {realEstate.totalArea}m²{" "}
+                          <FontAwesomeIcon icon={faRuler} />
+                        </p>
+                        <div className="text-left">
+                          <h5 className="text-primary">
+                            R$ {realEstate.salePrice}
+                          </h5>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
+                ))}
+          </div>
         </div>
       </div>
       <FooterContact />
