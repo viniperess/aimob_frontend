2291bbf8b627cf58814fa901cc1b974518e4ede8
Vinicius Peres <viniciusgperes@hotmail.com>
2024-11-17 16:29:06 -0300

    fix: new function for the disponible

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,11 +11,22 @@ import { faBed, faShower, faRuler } from "@fortawesome/free-solid-svg-icons";
 const Home: React.FC = () => {
   const [realEstates, setRealEstates] = useState<RealEstateType[]>([]);
   const [loading, setLoading] = useState(true);
+  const [selectedRealEstates, setSelectedRealEstates] = useState<Set<number>>(
+    new Set()
+  );
 
   const getRealEstates = async () => {
     try {
       const response = await api.get("realestates");
-      setRealEstates([...response.data]);
+      const sortedRealEstates = response.data.sort(
+        (a: RealEstateType, b: RealEstateType) => {
+          if (a.status === b.status) {
+            return b.viewsCount - a.viewsCount;
+          }
+          return a.status ? -1 : 1;
+        }
+      );
+      setRealEstates(sortedRealEstates);
     } catch (error) {
       console.log(error);
     } finally {
@@ -26,6 +37,28 @@ const Home: React.FC = () => {
   useEffect(() => {
     getRealEstates();
   }, []);
+  const toggleSelection = (id: number) => {
+    const updatedSelection = new Set(selectedRealEstates);
+    if (updatedSelection.has(id)) {
+      updatedSelection.delete(id);
+    } else {
+      updatedSelection.add(id);
+    }
+    setSelectedRealEstates(updatedSelection);
+  };
+
+  const updateRealEstateStatus = async (status: boolean) => {
+    try {
+      const promises = Array.from(selectedRealEstates).map((id) =>
+        api.patch(`/realestates/${id}`, { status })
+      );
+      await Promise.all(promises);
+      getRealEstates(); // Refresh the list after updating
+      setSelectedRealEstates(new Set()); // Clear selection
+    } catch (error) {
+      console.error("Erro ao atualizar status:", error);
+    }
+  };
 
   return (
     <>
@@ -37,75 +70,104 @@ const Home: React.FC = () => {
             Adicionar Imóvel
           </a>
         </div>
+        {selectedRealEstates.size > 0 && (
+          <div className="d-flex justify-content-end m-3" style={{fontSize: '12px'}}>
+            <button
+              onClick={() => updateRealEstateStatus(true)}
+              className="btn btn-success me-2"
+              style={{fontSize: '12px'}}
+            >
+              Marcar como Disponível
+            </button>
+            <button
+              onClick={() => updateRealEstateStatus(false)}
+              className="btn btn-danger"
+              style={{fontSize: '12px'}}
+            >
+              Marcar como Indisponível
+            </button>
+          </div>
+        )}
         <div className="main-content row m-3 mb-1 justify-content-center">
           {loading ? (
             [1, 2, 3, 4, 5, 6, 7, 8].map((n) => <Loading key={n} />)
           ) : realEstates.length === 0 ? (
             <p className="text-center w-100">Nenhum imóvel disponível.</p>
           ) : (
-            realEstates
-              .sort((a, b) => a.id - b.id)
-              .map((realEstate) => (
-                <div className="col mb-1" key={realEstate.id}>
-                  <div className="card">
-                    <a href={`/realEstate/${realEstate.id}`}>
-                      <img
-                        src={realEstate.images[0]}
-                        className="card-img"
-                        alt={realEstate.description}
-                      />
-                      <div className="card-img-overlay text-white d-flex justify-content-between align-items-start my-auto mx-auto">
+            realEstates.map((realEstate, index) => (
+              <div
+                className={`col mb-1 ${!realEstate.status ? "faded-card" : ""}`}
+                key={realEstate.id}
+              >
+                <div className="card">
+                <div className="position-absolute" style={{ top: "10px", left: "10px", zIndex: 1 }}>
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      checked={selectedRealEstates.has(realEstate.id)}
+                      onChange={() => toggleSelection(realEstate.id)}
+                    />
+                  </div>
+                  <a href={`/realEstate/${realEstate.id}`}>
+                    <img
+                      src={realEstate.images[0]}
+                      className="card-img"
+                      alt={realEstate.description}
+                    />
+                    <div className="card-img-overlay text-white d-flex justify-content-between align-items-start my-auto mx-auto">
+                      <p className="card-text-comprar">COMPRAR</p>
+                      {index < 3 && (
                         <h6 className="card-title-h5 text-white">DESTAQUE</h6>
-                        <p className="card-text-comprar">COMPRAR</p>
-                      </div>
-                    </a>
-                    <div className="card-body border secundary text-left">
-                      <h5 className="card-title text-left">
-                        {realEstate.description}
-                      </h5>
-                      <p className="card-text">
-                        <small className="text-body">
-                          Bairro {realEstate.district}
-                        </small>
+                      )}
+                    </div>
+                  </a>
+                  <div className="card-body border secundary text-left">
+                    <h5 className="card-title text-left">
+                      {realEstate.description}
+                    </h5>
+                    <p className="card-text">
+                      <small className="text-body">
+                        Bairro {realEstate.district}
+                      </small>
+                    </p>
+                    <div className="d-flex text-left">
+                      <p
+                        className="card-text"
+                        style={{ marginRight: "20px", opacity: 0.9 }}
+                      >
+                        <span className="m-1">
+                          <FontAwesomeIcon icon={faBed} />
+                        </span>
+                        {realEstate.bedrooms}
                       </p>
-                      <div className="d-flex text-left">
-                        <p
-                          className="card-text"
-                          style={{ marginRight: "20px", opacity: 0.9 }}
-                        >
-                          <span className="m-1">
-                            <FontAwesomeIcon icon={faBed} />
-                          </span>
-                          {realEstate.bedrooms}
-                        </p>
-                        <p
-                          className="card-text"
-                          style={{ marginRight: "20px", opacity: 0.9 }}
-                        >
-                          <span className="m-1">
-                            <FontAwesomeIcon icon={faShower} />
-                          </span>
-                          {realEstate.bathrooms}
-                        </p>
-                        <p
-                          className="card-text"
-                          style={{ marginRight: "20px", opacity: 0.9 }}
-                        >
-                          <span className="m-1">
-                            <FontAwesomeIcon icon={faRuler} />
-                          </span>
-                          {realEstate.totalArea}m²
-                        </p>
-                      </div>
-                      <div className="text-left">
-                        <h5 className="text-primary">
-                          R$ {realEstate.salePrice}
-                        </h5>
-                      </div>
+                      <p
+                        className="card-text"
+                        style={{ marginRight: "20px", opacity: 0.9 }}
+                      >
+                        <span className="m-1">
+                          <FontAwesomeIcon icon={faShower} />
+                        </span>
+                        {realEstate.bathrooms}
+                      </p>
+                      <p
+                        className="card-text"
+                        style={{ marginRight: "20px", opacity: 0.9 }}
+                      >
+                        <span className="m-1">
+                          <FontAwesomeIcon icon={faRuler} />
+                        </span>
+                        {realEstate.totalArea}m²
+                      </p>
+                    </div>
+                    <div className="text-left">
+                      <h5 className="text-primary">
+                        R$ {realEstate.salePrice}
+                      </h5>
                     </div>
                   </div>
                 </div>
-              ))
+              </div>
+            ))
           )}
         </div>
       </div>
