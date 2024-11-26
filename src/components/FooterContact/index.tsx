7a3c02bd349f49aab0b9b35f1d1b7a018b818221
Vinicius Peres <viniciusgperes@hotmail.com>
2024-11-26 15:03:05 -0300

    fix: update colors of the footer

--- src/components/FooterContact/index.tsx
+++ src/components/FooterContact/index.tsx
@@ -1,6 +1,12 @@
-import { faInstagram, faLinkedinIn, faGithub, faDiscord } from '@fortawesome/free-brands-svg-icons';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faInstagram,
+  faLinkedinIn,
+  faGithub,
+  faDiscord,
+  faFacebook,
+} from "@fortawesome/free-brands-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 library.add(faInstagram, faLinkedinIn, faGithub, faDiscord);
 
@@ -13,7 +19,7 @@ const FooterContact: React.FC = () => {
           {/* Section: Address */}
           <div className="col-lg-4 col-md-6 mb-4 mb-md-0 text-white">
             <h5 className="text-uppercase">Aimob</h5>
-            <p style={{color: "white"}}>
+            <p style={{ color: "white" }}>
               Endereço: Praça 20 de Setembro, 455 <br />
               Bairro Fragata, Cidade Pelotas, RS <br />
               Telefone: (53) 9999-9999 <br />
@@ -23,7 +29,9 @@ const FooterContact: React.FC = () => {
 
           {/* Section: Social media */}
           <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
-            <h5 className="text-uppercase" style={{color: "white"}}>Redes</h5>
+            <h5 className="text-uppercase" style={{ color: "white" }}>
+              Redes
+            </h5>
             <section className="mb-4">
               {/* Discord */}
               <a
@@ -37,12 +45,13 @@ const FooterContact: React.FC = () => {
 
               {/* Instagram */}
               <a
+                data-mdb-ripple-init
                 className="btn text-white btn-floating m-1"
-                style={{ backgroundColor: "#E4405F" }}
-                href="https://instagram.com/viniperess_"
+                style={{ backgroundColor: "#5993eb" }} // Cor oficial do Instagram
+                href="https://www.facebook.com/profile.php?id=61568643164281"
                 role="button"
               >
-                <FontAwesomeIcon icon={faInstagram} />
+                <FontAwesomeIcon icon={faFacebook} />
               </a>
 
               {/* Linkedin */}
@@ -70,16 +79,20 @@ const FooterContact: React.FC = () => {
           {/* Section: About */}
           <div className="col-lg-4 col-md-12 mb-4 mb-md-0 text-white">
             <h5 className="text-uppercase">Sobre</h5>
-            <p style={{color: "white"}}>
-              Aimob é uma plataforma dedicada a ajudar você a encontrar o imóvel dos seus sonhos. 
-              Com uma vasta seleção de imóveis à venda, garantimos que você encontrará o lar perfeito.
+            <p style={{ color: "white" }}>
+              Aimob é uma plataforma dedicada a ajudar você a encontrar o imóvel
+              dos seus sonhos. Com uma vasta seleção de imóveis à venda,
+              garantimos que você encontrará o lar perfeito.
             </p>
           </div>
         </div>
       </div>
 
       {/* Copyright */}
-      <div className="text-center p-3" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)", color: "white" }}>
+      <div
+        className="text-center p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)", color: "white" }}
+      >
         © 2024 Aimob - Todos os direitos reservados.
       </div>
     </footer>
